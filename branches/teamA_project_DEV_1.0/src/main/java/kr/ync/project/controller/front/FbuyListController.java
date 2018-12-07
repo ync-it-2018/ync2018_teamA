4fa8c62cdea7ee93be6669a7c4ff0e380e896ba8
team_a <team_a@260f0a35-2817-46b7-a138-34c9e64374ac>
2018-12-07 09:55:31 +0000

    #전형조 프론트 페이지 명 수정

--- branches/teamA_project_DEV_1.0/src/main/java/kr/ync/project/controller/front/FbuyListController.java
+++ branches/teamA_project_DEV_1.0/src/main/java/kr/ync/project/controller/front/FbuyListController.java
@@ -12,18 +12,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import kr.ync.project.controller.HomeController;
 
 @Controller
-public class buyListController {
+public class FbuyListController {
 
 private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 
-	@RequestMapping(value = "/buyList", method = RequestMethod.GET)
+	@RequestMapping(value = "/FbuyList", method = RequestMethod.GET)
 	public String about(Locale locale, Model model) {
 
 		// 가나다
-		logger.info("buyList로 이동", locale);
+		logger.info("FbuyList로 이동", locale);
 
-		return "front/buyList";
+		return "front/FbuyList";
 	}
 	
 }
